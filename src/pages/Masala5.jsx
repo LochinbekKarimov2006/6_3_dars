2bd99c4cb3f9822cebb3b0cb04df8b53f2d277d1
lochinbek <lochinbekk77@gmail.com>
2024-07-19 09:47:40 +0500

    5

--- src/pages/Masala5.jsx
+++ src/pages/Masala5.jsx
@@ -5,20 +5,16 @@ function Masala5() {
     let   width={
         width:`${uzunlik}%`,
     }
-    let toldi=''
     if(uzunlik==101){
         setUzunlik(100)
-      toldi='Toldi ++'
     }
     if(uzunlik==-0){
         setUzunlik(1)
-        toldi="Tugadi --"
-
+        
     }
-
   return (
     <div className='div-9'>
-        <h1 className='mb-4'>{toldi}</h1>
+        <h1 className='mb-4 text-3xl'>{uzunlik}%</h1>
         <div className='div-10'>
             <div className='w-56 bg-orange-900 h-5'><p className=' bg-orange-500 h-5' style={width}> </p></div>
         </div>
