b317f663619597714af97491b37ad0ec0ebcd02e
lochinbek <lochinbekk77@gmail.com>
2024-07-19 12:26:16 +0500

    hcch

--- src/pages/Masala2.jsx
+++ src/pages/Masala2.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Masala2() {
+    let [salom,setSalom]=useState('salom')
   return (
-    <div>Masala2</div>
+    <div>
+         <div className='div-5'>
+            <div className="flex w-full">
+                <button onClick={()=>{setSalom('Salom')}} className="card bg-base-300 rounded-box grid h-20 flex-grow place-items-center">
+                    Salom
+                </button>
+                <p className="divider divider-horizontal w-11">{salom}</p>
+                <button onClick={()=>{setSalom('Hayir')}} className="card bg-base-300 rounded-box grid h-20 flex-grow place-items-center">
+                    Hayir
+                </button>
+            </div>
+        </div>
+    </div>
   )
 }
 
